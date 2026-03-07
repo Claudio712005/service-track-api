2aefc1a553b8d6baec7cdae9b02df941f1bb5585
Claudio712005 <clausilvaaraujo11@gmail.com>
2026-03-07 09:26:20 -0300

    Commit inicial, app kotlin com Spring

---
+++ src/main/kotlin/br/com/claus/servicetrackapi/ServiceTrackApiApplication.kt
@@ -0,0 +1,11 @@
+package br.com.claus.servicetrackapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ServiceTrackApiApplication
+
+fun main(args: Array<String>) {
+    runApplication<ServiceTrackApiApplication>(*args)
+}
---
+++ src/test/kotlin/br/com/claus/servicetrackapi/ServiceTrackApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.com.claus.servicetrackapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ServiceTrackApiApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
